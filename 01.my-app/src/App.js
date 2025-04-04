06f6816fef724935ceeea940e5a79454fe8a4a04
Bumjun-hub <jc971017@gmail.com>
2025-04-04 17:42:16 +0900

    04/04 수업

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  // 주석
+
+  /*
+  여러줄 주석
+  */
+
+  return( 
+    <> {/* 여러줄 일때는 반드시 최상위 태그가 있어야함 */}
+      <h1>React Test Page</h1>
+      <p>오늘부터 리액트 시작</p>
+      <h2>ㅁㄴㅇㄹ</h2>
+    </>
+  )
+}
+
+export default App;
